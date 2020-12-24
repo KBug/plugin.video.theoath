934a60af4508d1dd65d2119567200f1ecbbd61e8
host505 <john_koudasgr@hotmail.com>
2020-12-24 10:27:47 +0200

    py3/k19 fix, cfscrape upd

--- resources/lib/modules/client.py
+++ resources/lib/modules/client.py
@@ -315,7 +315,7 @@ def request(url, close=True, redirect=True, error=False, verify=True, proxy=None
 
             if not as_bytes:
 
-                result = control.six_decode(result)
+                result = six.ensure_text(result, errors='ignore')
 
             return result, headers, content, cookie
 
@@ -367,7 +367,8 @@ def request(url, close=True, redirect=True, error=False, verify=True, proxy=None
             response.close()
 
         if not as_bytes:
-            result = control.six_decode(result)
+
+            result = six.ensure_text(result, errors='ignore')
 
         return result
 
